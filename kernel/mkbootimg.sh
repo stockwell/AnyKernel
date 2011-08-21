29ef751ec6492c4f84efe9d93e15f9d7781a467e
stockwell <stockwell@stockwell-965P-DS3P.(none)>
2011-08-21 13:41:11 +1000

    Modified for u8800, still not working though.

--- kernel/mkbootimg.sh
+++ kernel/mkbootimg.sh
@@ -1,6 +1,11 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --output /tmp/newboot.img >> /tmp/createnewboot.sh
+mkdir /cust
+mount cust
+cp /cust/image/boot.img /tmp/boot.img
+/tmp/unpackbootimg /tmp/boot.img /tmp/
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x00200000 --pagesize 4096 --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
+cp /tmp/newboot.img /cust/image/boot.img
 return $?
