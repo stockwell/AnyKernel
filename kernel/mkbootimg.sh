22662f74db56820016314406897fea8caaaff925
stockwell <stockwell@stockwell-965P-DS3P.(none)>
2011-08-22 21:26:51 +1000

    Finish modifications for u8800. - added newer version of unpackbootimg and mkbootimg which support different pagesizes - changed from using dump_image on /boot partition to just using image in /cust - modified mkbootimg.sh script to use pagesize from original boot image

--- kernel/mkbootimg.sh
+++ kernel/mkbootimg.sh
@@ -2,9 +2,8 @@
 echo \#!/sbin/sh > /tmp/createnewboot.sh
 mkdir /cust
 mount cust
-cp /cust/image/boot.img /tmp/boot.img
-/tmp/unpackbootimg /tmp/boot.img /tmp/
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x00200000 --pagesize 4096 --output /tmp/newboot.img >> /tmp/createnewboot.sh
+/tmp/unpackbootimg -i /cust/image/boot.img -o /tmp/
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --pagesize $(printf "%d" 0x`cat /tmp/boot.img-pagesize`) --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 cp /tmp/newboot.img /cust/image/boot.img
